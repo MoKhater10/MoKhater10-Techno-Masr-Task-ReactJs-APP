b52204a9fbd89d37c300dfdd85b3d4009838b73c
MoKhater10 <mostafa.khater17@gmail.com>
2023-03-14 16:00:08 +0200

    Second Uplod

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashBrowser } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashBrowser basename="/">
+    <BrowserRouter>
       <App />
-    </HashBrowser>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
